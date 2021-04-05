27c93299418b7594c673ccd52a0feece042ddd20
David Moina <davemolina777@gmail.com>
2021-04-05 12:49:47 -0500

    commit tree 3c060b42ab25f03e080ab3cc53eb8fc0f8e17f31

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'welcome/home'
+
+  get '/login' => 'sessions#new'
+
+  post '/login' => 'sessions#create'
+
+  post '/logout' => 'sessions#destroy'
+
+  resources :users, only: [:new, :create]
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
